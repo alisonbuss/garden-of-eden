ecc3d6d06a0262ab1a57819839213aeb165e167b
Alison Buss de Arruda <alisonbuss.dev@gmail.com>
2017-09-08 01:45:28 -0300

    ajuste foda

--- start-divine-creation-ui.sh
+++ start-divine-creation-ui.sh
@@ -1,52 +1,53 @@
 #!/bin/bash
 
 ###################  DOC  ###################
-# @descr: Script de instalação
+# @descr: Script de inicialização grafico do (Garden of Eden) para 
+#         provisionamento do ambiente básico de desenvolvimento. 
 # @example: 
-#    $ sudo chmod a+x install-environment.sh
-#    $ sudo bash install-environment.sh
-#  OR
-#    $ sudo bash install-environment.sh "install-golang.sh" '{ "version": "1.7.1", "op":"--remove" }'
-#  OR
-#    $ sudo bash install-environment.sh "list"
+#    $ sudo chmod a+x start-divine-creation-ui.sh
+#    $ sudo bash start-divine-creation-ui.sh
 #############################################
 
 source "./shell-script-tools/linux/utility.sh";
 source "./shell-script-tools/ubuntu/extension-jq.sh";
 
 function StartDivineCreationUI {
-
+    
     local error_command="";
     local file_path_menu="./files/commands.txt";
 
+    __stopAfterExecution() {
+        print.out '%s' "Comando executado! Presione [ENTER] para continuar..."; read;
+    }
+
     __runScript() {
-        print.info "__runScript";
-        sleep 2s;
+        bash start-divine-creation.sh "$@";
+        __stopAfterExecution;
     }
 
     __runAllScripts() {
-        print.info "__runAllScripts";
-        sleep 2s;
+        bash start-divine-creation.sh "--run-all";
+        __stopAfterExecution;
     }
 
     __listSettings() {
-        print.info "__listSettings";
-        sleep 2s;
+        bash start-divine-creation.sh "--list-set";
+        __stopAfterExecution;
     }
 
     __editSettings() {
-        print.info "__editSettings";
-        sleep 2s;
+        bash start-divine-creation.sh "$@";
+        __stopAfterExecution;
     }
 
     __editScript() {
-        print.info "__editScript";
-        sleep 2s;
+        bash start-divine-creation.sh "$@";
+        __stopAfterExecution;
     }
 
     __viewLog() {
-        print.info "__viewLog";
-        sleep 2s;
+        bash start-divine-creation.sh "$@";
+        __stopAfterExecution;
     }
 
     __viewDoc() {
@@ -56,43 +57,36 @@ function StartDivineCreationUI {
 
     __exit() {
         print.info "__exit";
-        exit;
+        exit 0;
     }
 
     __initialize() {
-        # This will clear the screen before displaying the menu.
         clear;
-
-        while : 
-        do
+        while [ true ]; do 
             #http://www.kammerl.de/ascii/AsciiSignature.php
             #http://patorjk.com/software/taag/#p=display&f=Graffiti&t=Type%20Something%20
             cat ./files/header.txt;
             cat $file_path_menu;
             file_path_menu="./files/commands.txt";
-
             # If error exists, display it
             if [ "$error_command" != "" ]; then
-                print.warning "Aviso: $error_command";
+                print.error "Error: $error_command";
                 # Clear the error message
                 error_command="";
             fi
-
             # Read the user input
-            print.out '%s' "O que tu queres fazer? $ "; read input_command;
-
-            case $input_command in
-                --run) __runScript; ;;
+            print.out '%s' "O que tu queres fazer? $ "; read -a input_commands;
+            case ${input_commands[0]} in
+                --run) __runScript "${input_commands[@]}"; ;;
                 --run-all) __runAllScripts; ;;
                 --list-set) __listSettings; ;;
-                --edit-set) __editSettings; ;;
-                --edit-script) __editScript; ;;
-                --view-log) __viewLog; ;;
+                --edit-set) __editSettings "${input_commands[@]}"; ;;
+                --edit-script) __editScript "${input_commands[@]}"; ;;
+                --view-log) __viewLog "${input_commands[@]}"; ;;
                 --view-doc) __viewDoc; ;;
                 --exit) __exit; ;;
                 *) error_command="Seu burro! Digite a porra do comando certo!";
             esac
-
             # This will clear the screen so we can redisplay the menu.
             clear;
         done
@@ -101,6 +95,15 @@ function StartDivineCreationUI {
     __initialize;
 } 
 
-StartDivineCreationUI;
+# Notificar usuario caso não exista o "jq" instalado na maquina, para instalar o "jq".
+if [ `isInstalled "jq"` == 1 ]; then
+    StartDivineCreationUI;
+else
+    print.warning "Aviso: É necessario ter instalado a extenção 'jq' na maquina!";
+    print.out '%s' "Deseja que o Garden of Eden instale? [yes/no] $ "; read input_install_jq;
+    if [ "$input_install_jq" == "yes" ]; then
+        installExtensionJQ;
+    fi
+fi
 
-exit 0
+exit 0;
--- start-divine-creation.sh
+++ start-divine-creation.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 ###################  DOC  ###################
-# @descr: Script de instalação
+# @descr: Script de inicialização do (Garden of Eden) para 
+#         provisionamento do ambiente básico de desenvolvimento. 
 # @example: 
 #    $ sudo chmod a+x install-environment.sh
 #    $ sudo bash install-environment.sh
@@ -15,25 +16,96 @@ source "./shell-script-tools/linux/utility.sh";
 source "./shell-script-tools/ubuntu/extension-jq.sh";
 
 function StartDivineCreation {
-    local paramScript="$1";
-    local paramOfScript="$2";
+    # Converter os parametros em um array.
+    declare -a parameters=("$@");
     local pathDefault=$(cat settings.json | jq -r '.pathDefault');
 
-    __initialize() {
-        # Caso não exista o "jq" instalado na maquina, instale o "jq".
-        if [ `isInstalled "jq"` == 1 ]; then
-            msgInfo "A extenção 'jq' já está instalanda na maquina...";
-        else
-            msgInfo "Instalando a extenção 'jq' na maquina...";
-            installExtensionJQ;
+    __execute() {
+        local script="$1";
+        local action="$2";
+        local param="$3";
+        local pathScript="$pathDefault/scripts/$script";
+        # chamar o shell-script e gerar log dela
+        ( exec $pathScript $action $param | tee -a ./$pathDefault/logs/$script.log );
+    }
+
+    __runScript() {
+        local script="${parameters[1]}";
+        local action="${parameters[2]}";
+        local param="${parameters[3]}";
+
+       __execute $script $action $param;
+    }
+
+    __runAllScripts() {
+        for row in $(cat settings.json | jq -c '.settings[]'); do    
+            local script=$(echo ${row} | jq -r '.script');  
+            local action=$(echo ${row} | jq '.action');
+            local param=$(echo ${row} | jq -c '.param');
+
+            local execute=$(echo ${row} | jq '.execute');
+            if [ "${execute}" == "true" ]; then
+                __execute $script $action $param;
+            fi
+        done 
+    }
+
+    __listSettings() {
+        cat ./settings.json;
+    }
+
+    __editSettings() {
+        local editor="${parameters[1]}";
+        if [ "${editor}" == "" ]; then
+            ( exec gedit ./settings.json );
         fi
- 
+        ( exec $editor ./settings.json );
+    }
+
+    __editScript() {
+        local script="$pathDefault/scripts/${parameters[1]}";
+        local editor="${parameters[2]}gedit";
+        if [ "${editor}" == "" ]; then
+            ( exec gedit $script );
+        fi
+        ( exec $editor $script );
+    }
+
+    __viewLog() {
+        local script="$pathDefault/logs/${parameters[1]}.log";
+        local editor="${parameters[2]}";
+        if [ "${editor}" == "" ]; then
+            ( exec gedit $script );
+        fi
+        ( exec $editor $script );
+    }
+
+    __initialize() {
+        local action="${parameters[0]}";
+        case $action in
+            --run) __runScript; ;;
+            --run-all) __runAllScripts; ;;
+            --list-set) __listSettings; ;;
+            --edit-set) __editSettings; ;;
+            --edit-script) __editScript; ;;
+            --view-log) __viewLog; ;;
+            *) print.error "Error: Tipo da (AÇÃO) não encontrada! [--run|--run-all|--list-set|--edit-set|--edit-script|--view-log]!";
+        esac
     }
 
     __initialize;
 } 
 
-# chamar a função e gerar log dela
-StartDivineCreation $@ | tee -a ./${0##*/}.log;
+# Notificar usuario caso não exista o "jq" instalado na maquina, para instalar o "jq".
+if [ `isInstalled "jq"` == 1 ]; then
+    # chamar a função e gerar log dela
+    StartDivineCreation $@ | tee -a ./${0##*/}.log;
+else
+    print.warning "Aviso: É necessario ter instalado a extenção 'jq' na maquina!";
+    print.out '%s' "Deseja que o Garden of Eden instale? [yes/no] $ "; read input_install_jq;
+    if [ "$input_install_jq" == "yes" ]; then
+        installExtensionJQ;
+    fi
+fi
 
 exit 0
