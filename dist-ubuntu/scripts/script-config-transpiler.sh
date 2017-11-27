dd2de7a3e6ea5ba6400a8278474a6e82999af62e
alisonbuss <alisonbuss.dev@gmail.com>
2017-11-27 06:59:47 -0300

    implementado o script de instalação do config-transpiler do CoreOS

--- dist-ubuntu/scripts/script-config-transpiler.sh
+++ dist-ubuntu/scripts/script-config-transpiler.sh
@@ -7,7 +7,9 @@
 # @fonts: https://coreos.com/os/docs/latest/provisioning.html
 #         https://coreos.com/os/docs/1478.0.0/overview-of-ct.html
 # @example:
-#       bash script-config-transpiler.sh --action='install' --param='{}'   
+#       bash script-config-transpiler.sh --action='install' --param='{"version":"0.5.0"}'   
+#   OR
+#       bash script-config-transpiler.sh --action='uninstall' --param='{}'  
 #-------------------------------------------------------------#
 
 source <(wget --no-cache -qO- "https://raw.githubusercontent.com/alisonbuss/shell-script-tools/master/import.sh"); 
@@ -17,16 +19,39 @@ import.ShellScriptTools "/linux/utility.sh";
 # @descr: Função principal do script-config-transpiler.sh
 # @param: 
 #    action | text: (install)
+#    param | json: '{"version":"..."}'
 function ScriptConfigTranspiler {
     
     # @descr: Variavel que define a ação que o script ira realizar.
     local ACTION=$(util.getParameterValue "(--action=|-a=)" "$@");
 
+    # @descr: Variavel de parametros JSON.
+    local PARAM_JSON=$(util.getParameterValue "(--param=|-p=)" "$@");
+
+    # @descr: Variavel da versão de instalação.
+    local version=$(echo ${PARAM_JSON} | jq -r '.version');
+
     # @descr: Função de instalação.
     __install() {
         util.print.info "Iniciando a instalação do ConfigTranspiler na maquina..."; 
 
-       
+        local urlDownload="https://github.com/coreos/container-linux-config-transpiler/releases/download";    
+
+        wget "${urlDownload}/v${version}/ct-v${version}-x86_64-unknown-linux-gnu" -O "./binaries/ct";
+        chmod -R 777 "./binaries/ct";
+
+        mv "./binaries/ct" "/usr/local/bin"
+
+        util.print.out '%s' "Version ConfigTranspiler: ";
+        ct --version;
+        util.print.out '%s\n' "";
+    }
+
+    # @descr: Função de desinstalação.
+    __uninstall() {
+        util.print.info "Iniciando a desinstalação do ConfigTranspiler na maquina..."; 
+        
+        rm "/usr/local/bin/ct";
     }
 
     # @descr: Função é chamada qndo a um erro de tipo de ação.
@@ -34,7 +59,7 @@ function ScriptConfigTranspiler {
     #    action | text: "..." | Action não encontrado.
     __actionError() {
         local actionErr=$(util.getParameterValue "(--action=|-a=)" "$@");
-        util.print.error "Erro: 'action' passado:(${actionErr}) não coincide com [install]!";
+        util.print.error "Erro: 'action' passado:(${actionErr}) não coincide com [install, uninstall]!";
         return 1;
     } 
 
@@ -43,6 +68,9 @@ function ScriptConfigTranspiler {
         case ${ACTION} in
             install) { 
                 __install; 
+            };;
+            uninstall) { 
+                __uninstall;
             };;
             *) {
                __actionError "--action=${ACTION}"; 
