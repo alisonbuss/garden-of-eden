4407fd6acefbea5b9647f060916768f94549e0e4
alisonbuss <alisonbuss.dev@gmail.com>
2019-05-19 03:54:16 -0300

    Ajuste

---
+++ shell-script/dist-ubuntu/personalize/script-tools.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+ 
+#-----------------------|DOCUMENTATION|-----------------------#
+# @descr: Instalação de Ferramentas comuns na maquina 
+# @fonts: https://vitux.com/linux-tree-command/
+# @example:
+#       bash script-tools.sh --action='install' --param='{}'   
+#-------------------------------------------------------------#
+
+# @descr: Função principal do script-tools.sh 
+# @param: 
+#    action | text: (install)
+function ScriptTools {
+    
+    # @descr: Variavel que define a ação que o script ira realizar.
+    local ACTION=$(util.getParameterValue "(--action=|-a=)" "$@");
+
+    # @descr: Função de instalação.
+    __install() {
+        util.print.out '%s\n' "Iniciando a instalação das Ferramentas na maquina..."; 
+
+        apt-get install tree;
+    }
+
+    # @descr: Função é chamada qndo a um erro de tipo de ação.
+    # @param: 
+    #    action | text: "..." | Action não encontrado.
+    __actionError() {
+        local actionErr=$(util.getParameterValue "(--action=|-a=)" "$@");
+        util.print.error "Erro: 'action' passado:(${actionErr}) não coincide com [install]!";
+        return 1;
+    } 
+
+    # @descr: Função principal "um construtor por exemplo".
+    __initialize() {
+        case ${ACTION} in
+            install) { 
+                __install; 
+            };;
+            *) {
+               __actionError "--action=${ACTION}"; 
+            };;
+        esac
+    }
+
+    # @descr: Chamada da função principal de inicialização do script.
+    __initialize;
+}
+
+# SCRIPT INITIALIZE...
+util.try; ( ScriptTools "$@" ); util.catch || {
+    util.print.error "Erro: Ao executar o script '${0##*/}', Exception Code: ${exception}";
+    util.throw $exception;
+}
+
+exit 0;
